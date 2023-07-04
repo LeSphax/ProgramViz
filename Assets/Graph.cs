d4793eb1bbaed6f5c534a4afca4f541af0b1b108
LeSphax <sbkerbrat@gmail.com>
2023-07-04 11:48:21 -0700

    Something

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -33,6 +33,7 @@ public class Graph : MonoBehaviour
         for (int i = 0; i < parsedModule.dependencies.Length; i++)
         {
             string name = parsedModule.dependencies[i].resolved;
+            if (name.StartsWith("src/types")) continue;
             if (existingModules.ContainsKey(name))
             {
                 dependencies.Add(existingModules[name]);
@@ -76,6 +77,7 @@ public class Graph : MonoBehaviour
 
     void Awake()
     {
+        //  npx depcruise --config .dependency-cruiser.js -x node_modules --output-type json src > deps.json
         //Parse the JSON file deps.json
         Timing.Start("Read JSON");
         string json = System.IO.File.ReadAllText("Assets/deps.json");
@@ -84,23 +86,52 @@ public class Graph : MonoBehaviour
         Content d = JsonConvert.DeserializeObject<Content>(json);
         Timing.Stop("Parse JSON");
         Timing.Start("Modules");
-        modules.Add(new Module("root", index));
-        index++;
+        //modules.Add(new Module("root", index));
+        //index++;
         for (int i = 0; i < d.modules.Length; i++)
         {
             ParsedModule module = d.modules[i];
             if (module.source != null && !existingModules.ContainsKey(module.source))
             {
-                //if (module.source.StartsWith("src/types")) continue;
+                if (module.source.StartsWith("src/types")) continue;
                 addModule(d.modules[i], d.modules);
             }
         }
         Debug.Log(d.modules.Length);
-        modules[0].dependencies.Concat(modules.Skip(1));
+        //modules[0].dependencies.Concat(modules.Skip(1));
         Timing.Stop("Modules");
+        return;
+        Timing.Start("GameObjects");
+        for (int i = 0; i < modules.Count; i++)
+        {
+            GameObject instance = Instantiate(node, randomStartPos(), Quaternion.identity);
+            instance.name = modules[i].source;
+            if (i == 0)
+            {
+                instance.GetComponent<Renderer>().material.color = Color.black;
+            }
+                else if (modules[i].dependents.Count == 0)
+            {
+                instance.GetComponent<Renderer>().material.color = Color.red;
+            }
+            else if (modules[i].dependencies.Count == 0)
+            {
+                instance.GetComponent<Renderer>().material.color = Color.green;
+            }
+            else
+            {
+                instance.GetComponent<Renderer>().material.color = Color.blue;
+            }
+
+            //instance.GetComponentInChildren<Canvas>().worldCamera = Camera.main;
+            //var split = modules[i].source.Split('/');
+            //instance.GetComponentInChildren<Text>().text = split[split.Length - 1];
+            modules[i].node = instance;
+        }
+        Timing.Stop("GameObjects");
     }
 
-    Vector3 attract(Vector3 currentPos, Vector3 targetPos, bool draw = false)
+    Vector3 attract(Vector3 currentPos, Vector3 targetPos, bool draw = true)
     {
         Timing.Start("Distance");
         float distance = Vector3.Distance(currentPos, targetPos);
@@ -118,65 +149,66 @@ public class Graph : MonoBehaviour
         Timing.Start("Force");
         Vector3 force = Mathf.Min(distance * distance / idealDistance, distance) * direction;
         Timing.Batch("Force");
-        //if (draw) DrawArrow.ForDebug(currentPos, targetPos);
+        if (draw) DrawArrow.ForDebug(currentPos, targetPos);
         return force;
     }
 
     void Update()
     {
-        //Timing.Start("Repulse");
-        //for (int i = 0; i < modules.Count; i++)
-        //{
-        //    Module current = modules[i];
-        //    Vector3 currentChange = Vector3.one * 0.00001f;
-        //    Vector3 currentPos = current.node.transform.position;
+        return;
+        Timing.Start("Repulse");
+        for (int i = 0; i < modules.Count; i++)
+        {
+            Module current = modules[i];
+            Vector3 currentChange = Vector3.zero;
+            Vector3 currentPos = current.node.transform.position;
 
-        //    //for (int j = 0; j < modules.Count; j++)
-        //    //{
-        //    //    if (i == j) continue;
+            for (int j = 0; j < modules.Count; j++)
+            {
+                if (i == j) continue;
 
-        //    //    //Vector3 targetPos = modules[j].node.transform.position;
-        //    //    //Vector3 direction = (currentPos - targetPos).normalized;
-        //    //    //if (direction.magnitude < 0.5f)
-        //    //    //{
-        //    //    //    direction = randomStartPos();
-        //    //    //}
-        //    //    //float distance = Vector3.Distance(currentPos, targetPos);
-        //    //    //Vector3 force = (idealDistance * idealDistance / (distance * distance)) * direction;
-        //    //    currentChange += Vector3.one * 0.00001f;
-        //    //}
+                Vector3 targetPos = modules[j].node.transform.position;
+                Vector3 direction = (currentPos - targetPos).normalized;
+                if (direction.magnitude < 0.5f)
+                {
+                    direction = randomStartPos();
+                }
+                float distance = Vector3.Distance(currentPos, targetPos);
+                Vector3 force = (idealDistance * idealDistance / (distance * distance)) * direction;
+                currentChange += force;
+            }
 
 
-        //    Timing.Start("Attract Loop");
-        //    for (int j = 0; j < current.dependencies.Count; j++)
-        //    {
+            Timing.Start("Attract Loop");
+            for (int j = 0; j < current.dependencies.Count; j++)
+            {
 
-        //        Vector3 targetPos = current.dependencies[j].node.transform.position;
-        //        currentChange += attract(currentPos, targetPos, true);
-        //    }
-        //    Timing.Batch("Attract Loop");
+                Vector3 targetPos = current.dependencies[j].node.transform.position;
+                currentChange += attract(currentPos, targetPos, true);
+            }
+            Timing.Batch("Attract Loop");
 
-        //    //for (int j = 0; j < current.dependents.Count; j++)
-        //    //{
-        //    //    Vector3 targetPos = current.dependents[j].node.transform.position;
-        //    //    currentChange += attract(currentPos, targetPos);
-        //    //}
+            for (int j = 0; j < current.dependents.Count; j++)
+            {
+                Vector3 targetPos = current.dependents[j].node.transform.position;
+                currentChange += attract(currentPos, targetPos);
+            }
 
-        //    //if (current.dependents.Count == 0)
-        //    //{
-        //    //    currentChange += attract(currentPos, Vector3.zero, true);
-        //    //}
+            if (current.dependents.Count == 0)
+            {
+                currentChange += attract(currentPos, Vector3.zero, true);
+            }
 
-        //    currentChange = currentChange.normalized * Mathf.Min(currentChange.magnitude, idealDistance * 3);
-        //    current.node.transform.position += currentChange * Time.deltaTime * speed;
+            currentChange = currentChange.normalized * Mathf.Min(currentChange.magnitude, idealDistance * 3);
+            current.node.transform.position += currentChange * Time.deltaTime * speed;
 
-        //    current.previousAcc = currentChange;
-        //}
-        //Timing.StopBatch("Distance");
-        //Timing.StopBatch("Force");
-        //Timing.StopBatch("Direction");
-        //Timing.StopBatch("Attract Loop");
-        //Timing.Stop("Repulse");
+            current.previousAcc = currentChange;
+        }
+        Timing.StopBatch("Distance");
+        Timing.StopBatch("Force");
+        Timing.StopBatch("Direction");
+        Timing.StopBatch("Attract Loop");
+        Timing.Stop("Repulse");
     }
 }
 
